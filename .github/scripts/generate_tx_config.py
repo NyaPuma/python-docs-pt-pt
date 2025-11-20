bf0f417f7bc1203f6ef0c4c0bd02cf4db8ad3907
NyaPuma <afrmscb@gmail.com>
2025-11-20 15:13:39 +0000

    Update generate_tx_config.py

--- .github/scripts/generate_tx_config.py
+++ .github/scripts/generate_tx_config.py
@@ -1,87 +1,125 @@
-"""Please note that this script requires a Transifex API token to run."""
-import glob
-import subprocess
-from functools import partial
-from pathlib import Path
+#!/usr/bin/env python3
+"""
+Generate the .tx/config file based on the existing projects
+in Python docs Transifex project. Takes an project slug as
+positional argument, e.g. python-newest or python-313
+"""
+
+import argparse
 import re
-import os
+import subprocess
+import sys
+from pathlib import Path
 
-run = partial(subprocess.run, check=True)
+# Replaces required to fix the default values set by 'tx add remote' command.
+# Add or remove
+TEXT_TO_REPLACE = {
+    "2_": "2.",
+    "3_": "3.",
+    "glossary_": "glossary",
+    "collections_": "collections.",
+    "compression_": "compression.",
+    "concurrent_": "concurrent.",
+    "curses_": "curses.",
+    "email_": "email.",
+    "html_": "html.",
+    "http_": "http.",
+    "importlib_resources_": "importlib.resources.",
+    "importlib_": "importlib.",
+    "logging_": "logging.",
+    "multiprocessing_": "multiprocessing.",
+    "os_": "os.",
+    "string_": "string.",
+    "sys_monitoring": "sys.monitoring",
+    "tkinter_": "tkinter.",
+    "unittest_": "unittest.",
+    "urllib_": "urllib.",
+    "xml_dom_": "xml.dom.",
+    "xml_etree_": "xml.etree.",
+    "xmlrpc_": "xmlrpc.",
+    "xml_sax_": "xml.sax.",
+    "xml_": "xml.",
+}
 
 
-def init_project():
-    run(["tx", "init"])
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--root-path",
+        "-p",
+        default=Path("."),
+        help="Path to the translation files, and also the .tx/config (defaults to current directory)",
+    )
+    parser.add_argument(
+        "tx_project", help="Slug of the Transifex project to query resources from"
+    )
+    return parser.parse_args()
 
 
-def add_files(project_name: str):
-    run(
+def reset_tx_config(txconfig: Path):
+    """Create or reset the .tx/config file with basic header."""
+    txconfig.parent.mkdir(exist_ok=True)
+    txconfig.write_text("[main]\nhost = https://www.transifex.com\n", encoding="utf-8")
+    print("Initialized .tx/config.")
+
+
+def populate_resources_from_remote(config_file: Path, tx_project: str):
+    """Add the remote resources from the Transifex project to .tx/config."""
+    result = subprocess.run(
         [
             "tx",
+            "--config",
+            str(config_file),
             "add",
             "remote",
             "--file-filter",
-            "trans/<lang>/<resource_slug>.<ext>",
-            f"https://www.transifex.com/python-doc/{project_name}/dashboard/",
-        ]
+            "<lang>/<resource_slug>.<ext>",
+            f"https://app.transifex.com/python-doc/{tx_project}/",
+        ],
+        check=True,
     )
+    if result.returncode != 0:
+        print("Failed to add the resources from remote:")
+        print(result.stderr.strip())
+        sys.exit(result.returncode)
+    print("Added remote resources to Transifex.")
 
 
-FILTER_PATTERN = re.compile(
-    r"^(?P<prefix>file_filter( *)=( *))(?P<resource>.+)$", re.MULTILINE
-)
+def patch_config(txconfig: Path):
+    """Patch .tx/config to fixing PO filenames to match the expected."""
+    content = txconfig.read_text(encoding="utf-8").splitlines()
+    new_lines = []
+
+    for line in content:
+        if line.startswith(("source_file", "source_lang")):
+            continue
+
+        if line.startswith("file_filter"):
+            line = line.replace("<lang>/", "")
+            line = line.replace("--", "/")
+
+            for pattern, replacement in TEXT_TO_REPLACE.items():
+                if pattern in line:
+                    line = line.replace(pattern, replacement)
+                    break
+
+        new_lines.append(line)
+
+    text = "\n".join(new_lines)
+
+    txconfig.write_text(text + "\n", encoding="utf-8")
+    print("Updated .tx/config with character substitutions")
 
 
-def name_replacer(match: re.Match[str]):
-    prefix, resource = match.group("prefix", "resource")
-    override_prefix = prefix.replace("file_filter", "trans.pt_PT")
-    pattern = (
-        resource.replace("trans/<lang>/", "")
-        .replace("glossary_", "glossary")
-        .replace("--", "/")
-        .replace("_", "?")
-    )
-    matches = list(glob.glob(pattern.replace(".po", ".rst")))
-    if not matches:
-        print("missing", pattern)
-        return f"{prefix}{resource}\n{override_prefix}{pattern.replace('?', '_')}"
-    elif len(matches) == 1:
-        filename = matches[0].replace(".rst", ".po").replace("\\", "/")
-    else:
-        raise ValueError("multi match", resource, pattern, matches)
-    return f"{prefix}{resource}\n{override_prefix}{filename}"
-
-
-def patch_config(path: str):
-    tx_config_path = Path(".tx", "config")
-
-    config_content = tx_config_path.read_text("utf-8")
-
-    cwd = os.getcwd()
-    os.chdir(path)
-    config_content = FILTER_PATTERN.sub(name_replacer, config_content)
-    config_content = re.sub(r'replace_edited_strings.*\n','', config_content)
-    config_content = re.sub(r'keep_translations.*\n','', config_content)
-    config_content = re.sub(r'0\ntrans\.pt_PT.*\n','0\n', config_content)
-    config_content = config_content.replace('         =','=')
-    os.chdir(cwd)
-
-    tx_config_path.write_text(config_content, "utf-8")
+def main():
+    args = parse_args()
+    config_path = Path(".tx/config")
+    if args.root_path:
+        config_path = args.root_path / config_path
+    reset_tx_config(config_path)
+    populate_resources_from_remote(config_path, args.tx_project)
+    patch_config(config_path)
 
 
 if __name__ == "__main__":
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-
-    parser.add_argument("--token", default="")
-    parser.add_argument("--project-name", required=True)
-    parser.add_argument("--doc-path", required=True)
-
-    params = parser.parse_args()
-
-    if params.token:
-        os.environ["TX_TOKEN"] = params.token
-
-    init_project()
-    add_files(params.project_name)
-    patch_config(params.doc_path)
+    main()
